009ddb42e3afdf96a5f74d8ca4291edd920d3dd6
michael <mjbosher@yahoo.co.uk>
2024-11-08 20:34:20 +0000

    amended setup to take in consideration that root will be running the script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,4 @@
+user=$USER
 #SET-UP ZeroTier for ftp
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg' | gpg --import && \  
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
@@ -16,13 +17,14 @@ sudo apt install python3-pip
 python3.11 -m pip install pyftpdlib
 
 #install dependency requirements
-python3.11 -m pip install -r $HOME/Fiber-optic-project/requirements.txt
+requirements=$HOME/Fiber-optic-project/requirements.txt
+sudo python3.11 -m pip install -r requirements
 
 #copy service so that the system can see it
 sudo cp /etc/systemd/system /etc/systemd/system
 
 #Change filepath in dashboard.service to point directly at  the user who installs it , which should be the same user that runs it
-sed -i 's/$user/'$USER/ /etc/systemd/dashboard.service
+sed -i 's/$user/'$user/ /etc/systemd/dashboard.service
 
 #reload systemd to see the copied file
 sudo systemctl daemon-reload
