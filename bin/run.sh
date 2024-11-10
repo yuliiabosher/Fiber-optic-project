d07cb0a40dca329bf4201234f6a1e7d83c0c15bb
michael <mjbosher@yahoo.co.uk>
2024-11-10 12:51:11 +0000

    temporary , commented out some code to make the EC2 drop its load for a bit , so I can inspect it

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,7 @@
 
 #pull any updates before starting the server
 branch="dashboard-example"
-git -C  /home/$user/Fiber-optic-project/ pull origin $branch
+#git -C  /home/$user/Fiber-optic-project/ pull origin $branch
 
 export FLASK_APP=/home/$user/Fiber-optic-project/app.py
-sudo flask run --host=0.0.0.0 --port=443
+#sudo flask run --host=0.0.0.0 --port=443
