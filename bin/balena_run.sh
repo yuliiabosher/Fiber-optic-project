d1805956da78c46effb218560213463e761771f8
michael <michael@pondus.xyz>
2024-11-11 11:22:10 +0000

    debugging

--- bin/balena_run.sh
+++ bin/balena_run.sh
@@ -2,12 +2,11 @@
 
 #pull any updates before starting the server
 branch="dashboard-example"
-echo "HERE"
 git -C  /var/www/html/ pull origin $branch
 
 #mount drive with data files on it
 mkdir files/data
-mount /dev/sda1 /var/www/html/files/data
+mount /dev/sda /var/www/html/files/data
 
 #Start webserver
 python3.11 
