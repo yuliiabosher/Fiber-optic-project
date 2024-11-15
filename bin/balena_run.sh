51d0614341780a8b9075ea362461d42be1481328
michael <michael@pondus.xyz>
2024-11-15 19:12:13 +0000

    test

--- bin/balena_run.sh
+++ bin/balena_run.sh
@@ -8,7 +8,7 @@ git -C  /var/www/html/ pull origin $branch
 
 #mount drive with data files on it
 mkdir files/data
-mount /dev/sdb2 /var/www/html/files/data
+mount /dev/sda2 /var/www/html/files/data
 
 #Start webserver
 python3.11 app.py
