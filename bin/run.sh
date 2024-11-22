ac13d6a32491b3d60a1be23baf1514c23b9857d6
michael <michael@pondus.xyz>
2024-11-22 21:02:36 +0000

    test

--- bin/run.sh
+++ bin/run.sh
@@ -7,5 +7,5 @@ git -C  /home/$user/Fiber-optic-project/ pull origin $branch
 #docker build and run the dashboard, mounting a media driver/folder which holds the data files
 #expose port 443
 sudo docker build --no-cache -t dashboard . 
-sudo docker run -v /media/$user/$usb/data:/var/www/html/files/data:rshared -it -p 443:443 dashboard
+sudo docker run -v /home/$user/Downloads/bigfolder/DASHBOARD_DATA:/var/www/html/files/data:rshared -it -p 80:80 dashboard
 
