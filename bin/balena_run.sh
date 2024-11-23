8cac3c40d239e0f706306764da565b54be559618
michael <mjbosher@yahoo.co.uk>
2024-11-23 19:25:03 +0000

    removed mount in script due to having a named volume

--- bin/balena_run.sh
+++ bin/balena_run.sh
@@ -6,9 +6,5 @@ git -C  /var/www/html/ branch $branch #silently fail if branch already exists, n
 git -C  /var/www/html/ switch $branch
 git -C  /var/www/html/ pull origin $branch
 
-#mount drive with data files on it
-mkdir files/data
-mount /dev/sda2 /var/www/html/files/data
-
 #Start webserver
 python3.11 app.py
