e3c501a7b802e6ae30681a482ae5c2843135d30b
michael <michael@pondus.xyz>
2024-12-01 15:31:53 +0000

    fixed run script

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,7 @@
 
 #pull any updates before starting the server
 branch="dashboard-example"
-git -C  /home/$USERr/Fiber-optic-project/ pull origin $branch
-source /home/$USER/Fiber-optic-project/env/bin/activate
-python /home/$USER/Fiber-optic-project/app.py
+home=/home/$USER/Fiber-optic-project
+git -C  $home pull origin $branch
+source $home/env/bin/activate
+python $home/app.py
