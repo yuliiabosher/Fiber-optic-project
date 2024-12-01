66277c70d7ec9c930da3d3d1baaf7ea408b75960
michael <michael@pondus.xyz>
2024-12-01 20:09:17 +0000

    amended run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#pull any updates before starting the server
-sleep 5m
+#pull any updates before starting the serve
+sleep 1m
 branch="dashboard-example"
 home=/home/$USER/Fiber-optic-project
-git -C  $home pull origin $branch
-source $home/env/bin/activate
+cd $home
+git pull origin $branch
+source env/bin/activate
 
 2>/dev/null 1>python admin_api.py 2 &
-python $home/app.py
+python app.py
