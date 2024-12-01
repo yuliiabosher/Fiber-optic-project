1847e310d1c8f42a4858496a600f77310fbd54dd
michael <mjbosher@yahoo.co.uk>
2024-12-01 15:04:18 +0000

    Added new run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -3,9 +3,5 @@
 #pull any updates before starting the server
 branch="dashboard-example"
 git -C  /home/$user/Fiber-optic-project/ pull origin $branch
-
-#docker build and run the dashboard, mounting a media driver/folder which holds the data files
-#expose port 443
-sudo docker build --no-cache -t dashboard . 
-sudo docker run -v /home/$user/Downloads/bigfolder/DASHBOARD_DATA:/var/www/html/files/data:rshared -it -p 80:80 dashboard
-
+source /home/$user/Fiber-optic-project/env/bin/activate
+python /home/$user/Fiber-optic-project/app.py
