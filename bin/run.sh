d199384ec96afb019bd0ef5aedb5084c4f2c6d2f
michael <mjbosher@yahoo.co.uk>
2024-12-01 15:21:26 +0000

    changed  to michael in run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,6 @@
 
 #pull any updates before starting the server
 branch="dashboard-example"
-git -C  /home/$user/Fiber-optic-project/ pull origin $branch
-source /home/$user/Fiber-optic-project/env/bin/activate
-python /home/$user/Fiber-optic-project/app.py
+git -C  /home/$USERr/Fiber-optic-project/ pull origin $branch
+source /home/$USER/Fiber-optic-project/env/bin/activate
+python /home/$USER/Fiber-optic-project/app.py
