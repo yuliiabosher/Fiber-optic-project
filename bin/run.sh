65ec8e47f08e997a18aea96004de9268ce87ab1e
michael <mjbosher@yahoo.co.uk>
2024-12-01 19:09:15 +0000

    added admin api to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,6 @@ branch="dashboard-example"
 home=/home/$USER/Fiber-optic-project
 git -C  $home pull origin $branch
 source $home/env/bin/activate
+
+2>/dev/null 1>python admin_api.py 2 &
 python $home/app.py
